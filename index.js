9840539c1827cefe170055854e2a30cd2b2853fc
John O'Connor <oconnojb@gmail.com>
2018-09-19 00:31:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
-// function BoardMember(name, homeState, training){
-//   this.name = name
-//   this.homeState = homeState
-//   this.training = training
-//
-//   this.veto = function(){
-//     return 'No, I must disagree'
-//   }
+function BoardMember(name, homeState, training){
+  this.name = name
+  this.homeState = homeState
+  this.training = training
+}
+
+BoardMember.prototype.veto = function(){
+     return 'No, I must disagree'
+   }
 //   this.approve = function(){
 //     return 'You can do that!'
 //   }
